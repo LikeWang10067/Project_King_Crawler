99a038e8389ea44707002dfa838c6be52c453e6c
RickyCowboy <like.wang@mail.utoronto.ca>
2025-03-10 01:43:27 -0400

    refactor: re-organize the code into different fucntions

--- src/tickets_crawler.py
+++ src/tickets_crawler.py
@@ -3,11 +3,14 @@ from bs4 import BeautifulSoup
 import re
 import csv
 
-url = "https://issues.apache.org/jira/browse/CAMEL-10597"
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
+def init(url="https://issues.apache.org/jira/browse/CAMEL-10597"):
+    response = requests.get(url)
+    if response.status_code != 200:
+        print("Failed to fetch page")
+        exit(1)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    write_to_csv(data_crawling(soup))
 
-ticket_info = {}
 
 def extract_text(soup, label, title=None, id=None):
     element = soup.find(label, title=title, id=id)
@@ -22,43 +25,48 @@ def extract_text(soup, label, title=None, id=None):
             return [item.strip() for item in split_list]
     return None
 
-# extract all the Details
-details_list = ["Type", "Status", "Priority", "Resolution", "Affects Version/s", "Fix Version/s", "Component/s", "Labels", "Patch Info", "Estimated Complexity"]
-for detail in details_list:
-    ticket_info[detail] = extract_text(soup, "strong", detail)
+def data_crawling(soup):
+    ticket_info = {}
+    # extract all the Details
+    details_list = ["Type", "Status", "Priority", "Resolution", "Affects Version/s", "Fix Version/s", "Component/s", "Labels", "Patch Info", "Estimated Complexity"]
+    for detail in details_list:
+        ticket_info[detail] = extract_text(soup, "strong", detail)
 
-# extract all the People
-people_list = ["Assignee", "Reporter", "Votes", "Watchers"]
-for people in people_list:
-    ticket_info[people] = extract_text(soup, "dt", people)
+    # extract all the People
+    people_list = ["Assignee", "Reporter", "Votes", "Watchers"]
+    for people in people_list:
+        ticket_info[people] = extract_text(soup, "dt", people)
 
-# extract all the Dates
-dates_list = ["Created", "Updated", "Resolved"]
-for date in dates_list:
-    ticket_info[date] = extract_text(soup, "dt")
+    # extract all the Dates
+    dates_list = ["Created", "Updated", "Resolved"]
+    for date in dates_list:
+        ticket_info[date] = extract_text(soup, "dt")
 
-# extract the Description
-description = extract_text(soup, "div", None, "descriptionmodule_heading")
-ticket_info["Description"] = description
+    # extract the Description
+    description = extract_text(soup, "div", None, "descriptionmodule_heading")
+    ticket_info["Description"] = description
 
-# extract the Comments
-# comments = {}
-# pattern = re.compile(r"comment-\d{8}")
-# ind = "comment-15748543"
-# comments = soup.find_all("div", id=ind)
-# comments = soup.find_all("div", class_="activity-comment")
-# print(comments)
+    # extract the Comments
+    # comments = {}
+    # pattern = re.compile(r"comment-\d{8}")
+    # ind = "comment-15748543"
+    # comments = soup.find_all("div", id=ind)
+    # comments = soup.find_all("div", class_="activity-comment")
+    # print(comments)
 
-# comments_list = soup.find_all(id=ind)
-# for comment in comments_list:
-#     comments[comment.find("a", class_="user-hover").text] = comment.find("div", class_="action-body").text
-# print("Comments: " + str(comments))
-# ticket_info["Comments"] = comments
+    # comments_list = soup.find_all(id=ind)
+    # for comment in comments_list:
+    #     comments[comment.find("a", class_="user-hover").text] = comment.find("div", class_="action-body").text
+    # print("Comments: " + str(comments))
+    # ticket_info["Comments"] = comments
 
-print(ticket_info)
+    return ticket_info
 
-# save the ticket_info to a .csv file
-with open('ticket_info.csv', 'w') as csv_file:
-    writer = csv.writer(csv_file)
-    writer.writerow(ticket_info.keys())
-    writer.writerow(ticket_info.values())
+def write_to_csv(ticket_info):
+    with open('ticket_info.csv', 'w') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(ticket_info.keys())
+        writer.writerow(ticket_info.values())
+
+if __name__ == "__main__":
+    init()
